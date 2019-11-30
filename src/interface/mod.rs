3af203d4671aa1830be7408b5cd967a3b5df2868
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 15:42:29 -0700

    Stub for RowBuffer interface

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -2,8 +2,10 @@ pub(crate) mod field;
 pub(crate) mod metadata;
 pub(crate) mod metadata_row_groups;
 pub(crate) mod row;
+pub(crate) mod row_buffer;
 
 pub use field::*;
 pub use metadata::*;
 pub use metadata_row_groups::*;
 pub use row::*;
+pub use row_buffer::*;
