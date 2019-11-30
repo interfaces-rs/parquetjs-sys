37964417b48cf76c185752eecd0583da58f710cd
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 10:35:49 -0700

    Stub for Metadata interface

---
+++ src/interface/metadata.rs
@@ -0,0 +1,8 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen]
+    #[derive(Clone, Debug, PartialEq)]
+    pub type Metadata;
+}
--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,1 +1,3 @@
+pub(crate) mod metadata;
 
+pub use metadata::*;
