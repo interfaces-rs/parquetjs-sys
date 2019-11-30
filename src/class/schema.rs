3776c3c1315813bf37ec6028138e3782be1981a3
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 04:16:41 -0700

    Bindings for Schema class

--- src/class/schema.rs
+++ src/class/schema.rs
@@ -1,1 +1,26 @@
+use js_sys::Promise;
+use wasm_bindgen::prelude::*;
 
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_name = "ParquetSchema")]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    pub type Schema;
+
+    //*************//
+    // Constructor //
+    //*************//
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(schema: &JsValue) -> Schema;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method, js_name = "findField")]
+    pub fn find_field(path: &JsString) -> JsValue;
+
+    #[wasm_bindgen(method, js_name = "findFieldBranch")]
+    pub fn find_field_branch(path: &JsString) -> JsValue;
+}
