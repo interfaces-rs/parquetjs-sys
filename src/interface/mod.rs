c07ba061f51a5fdab2e82df645723e20000feb44
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 15:57:07 -0700

    Stub for Schema interface

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -3,6 +3,7 @@ pub(crate) mod metadata;
 pub(crate) mod metadata_row_groups;
 pub(crate) mod row;
 pub(crate) mod row_buffer;
+pub(crate) mod schema;
 pub(crate) mod writer_options;
 
 pub use field::*;
@@ -10,4 +11,5 @@ pub use metadata::*;
 pub use metadata_row_groups::*;
 pub use row::*;
 pub use row_buffer::*;
+pub use schema::*;
 pub use writer_options::*;
---
+++ src/interface/schema.rs
@@ -0,0 +1,9 @@
+use js_sys::Map;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(extends = Map)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    pub type Schema;
+}
