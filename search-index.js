018a9681a426142224bf6cacf4a90b8f3808ba04
darinmorrison <darinmorrison@users.noreply.github.com>
2019-11-30 10:46:19 +0000

    Automated deployment: Sat Nov 30 10:46:19 UTC 2019 5ff02e5b65213852716529ab643907e34e17cd2a

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+
+searchIndex["parquetjs_sys"]={"doc":"Raw bindings to the parquetjs API for projects using…","i":[],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["parquetjs_sys"] = {"name":"","dirs":[{"name":"class","files":["mod.rs"]},{"name":"interface","files":["mod.rs"]},{"name":"module","files":["mod.rs"]}],"files":["lib.rs"]};
+createSourceSidebar();
