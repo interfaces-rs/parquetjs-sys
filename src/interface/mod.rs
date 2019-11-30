ce7429289db2e5723fc7246e6b0ccc03a5a809e1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 15:43:12 -0700

    Stub for WriterOptions interface

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -3,9 +3,11 @@ pub(crate) mod metadata;
 pub(crate) mod metadata_row_groups;
 pub(crate) mod row;
 pub(crate) mod row_buffer;
+pub(crate) mod writer_options;
 
 pub use field::*;
 pub use metadata::*;
 pub use metadata_row_groups::*;
 pub use row::*;
 pub use row_buffer::*;
+pub use writer_options::*;
---
+++ src/interface/writer_options.rs
@@ -0,0 +1,9 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct WriterOptions {}
+
+#[wasm_bindgen]
+impl WriterOptions {
+}
