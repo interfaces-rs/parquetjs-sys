148668addc5ff468b208006ef602d902fef7e999
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 15:42:08 -0700

    Stub for MetadataRowGroups interface

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -1,7 +1,9 @@
 pub(crate) mod field;
 pub(crate) mod metadata;
+pub(crate) mod metadata_row_groups;
 pub(crate) mod row;
 
 pub use field::*;
 pub use metadata::*;
+pub use metadata_row_groups::*;
 pub use row::*;
