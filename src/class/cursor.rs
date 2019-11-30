00171640e12fa08bd4d9527f7002f1478fa377f1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 03:33:43 -0700

    Bindings for Cursor class

---
+++ src/class/cursor.rs
@@ -0,0 +1,26 @@
+use js_sys::Promise;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_name = "ParquetReader")]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    pub type Cursor;
+
+    //*************//
+    // Constructor //
+    //*************//
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(metadata: &JsValue, envelope_reader: &JsValue, schema: &JsValue, column_list: &JsValue) -> Cursor;
+
+    //******************//
+    // Instance Methods //
+    //******************//
+
+    #[wasm_bindgen(method)]
+    pub fn next(this: &Cursor) -> Promise;
+
+    #[wasm_bindgen(method)]
+    pub fn rewind(this: &Cursor);
+}
