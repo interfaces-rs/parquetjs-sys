68cf344dc5016924d1d78bda8d6ccbbfca3ee603
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 04:12:35 -0700

    Partial bindings for Transformer class

--- src/class/transformer.rs
+++ src/class/transformer.rs
@@ -1,1 +1,19 @@
 
+
+use crate::class::Cursor;
+use js_sys::Promise;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    #[wasm_bindgen(js_name = "ParquetTransformer")]
+    #[derive(Clone, Debug, Eq, PartialEq)]
+    pub type Transformer;
+
+    //*************//
+    // Constructor //
+    //*************//
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(schema: &JsValue, opts: &JsValue) -> Transformer;
+}
