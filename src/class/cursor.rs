e1edbaf4b7ae9acb9e3630db3a5aaf04bad60b0e
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 10:37:06 -0700

    Bindings for Cursor class

--- src/class/cursor.rs
+++ src/class/cursor.rs
@@ -1,4 +1,8 @@
-use js_sys::Promise;
+use crate::{
+    class::{EnvelopeReader, Schema},
+    interface::Metadata,
+};
+use js_sys::{Array, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -12,7 +16,7 @@ extern {
     //*************//
 
     #[wasm_bindgen(constructor)]
-    pub fn new(metadata: &JsValue, envelope_reader: &JsValue, schema: &JsValue, column_list: &JsValue) -> Cursor;
+    pub fn new(metadata: &Metadata, envelope_reader: &EnvelopeReader, schema: &Schema, column_list: &Array) -> Cursor;
 
     //******************//
     // Instance Methods //
@@ -23,4 +27,26 @@ extern {
 
     #[wasm_bindgen(method)]
     pub fn rewind(this: &Cursor);
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter, js_name = "columnList")]
+    pub fn column_list(this: &Cursor) -> Array;
+
+    #[wasm_bindgen(method, getter, js_name = "envelopeReader")]
+    pub fn envelope_reader(this: &Cursor) -> EnvelopeReader;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn metadata(this: &Cursor) -> Metadata;
+
+    #[wasm_bindgen(method, getter, js_name = "rowGroup")]
+    pub fn row_group(this: &Cursor) -> Array;
+
+    #[wasm_bindgen(method, getter, js_name = "rowGroupIndex")]
+    pub fn row_group_index(this: &Cursor) -> usize;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn schema(this: &Cursor) -> Schema;
 }
