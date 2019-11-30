9eb3c868500ef7c162c9e089ddd9632f299eb6f5
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 09:28:21 -0700

    Configure modules

--- src/class/mod.rs
+++ src/class/mod.rs
@@ -1,1 +1,17 @@
+pub(crate) mod cursor;
+pub(crate) mod envelope_reader;
+pub(crate) mod envelope_writer;
+pub(crate) mod reader;
+pub(crate) mod schema;
+pub(crate) mod shredder;
+pub(crate) mod transformer;
+pub(crate) mod writer;
 
+pub use cursor::*;
+pub use envelope_reader::*;
+pub use envelope_writer::*;
+pub use reader::*;
+pub use schema::*;
+pub use shredder::*;
+pub use transformer::*;
+pub use writer::*;
