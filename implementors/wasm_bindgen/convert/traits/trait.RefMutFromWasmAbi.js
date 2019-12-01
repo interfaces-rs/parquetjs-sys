12e2f9bfc8c6ba822c56ae706599ed012a6731d7
darinmorrison <darinmorrison@users.noreply.github.com>
2019-12-01 02:49:12 +0000

    Automated deployment: Sun Dec  1 02:49:12 UTC 2019 551022ededc69ddfb660dab0e9e715477aaaf295

---
+++ implementors/wasm_bindgen/convert/traits/trait.RefMutFromWasmAbi.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["parquetjs_sys"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/wasm-bindgen/0.2/wasm_bindgen/convert/traits/trait.RefMutFromWasmAbi.html\" title=\"trait wasm_bindgen::convert::traits::RefMutFromWasmAbi\">RefMutFromWasmAbi</a> for <a class=\"struct\" href=\"parquetjs_sys/struct.WriterOptions.html\" title=\"struct parquetjs_sys::WriterOptions\">WriterOptions</a>",synthetic:false,types:["parquetjs_sys::interface::writer_options::WriterOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["parquetjs_sys"] = {"name":"","dirs":[{"name":"class","files":["cursor.rs","envelope_reader.rs","envelope_writer.rs","mod.rs","reader.rs","schema.rs","shredder.rs","transformer.rs","writer.rs"]},{"name":"interface","files":["mod.rs"]},{"name":"module","files":["mod.rs"]}],"files":["lib.rs"]};
+sourcesIndex["parquetjs_sys"] = {"name":"","dirs":[{"name":"class","files":["cursor.rs","envelope_reader.rs","envelope_writer.rs","mod.rs","reader.rs","schema.rs","shredder.rs","transformer.rs","writer.rs"]},{"name":"interface","files":["field.rs","metadata.rs","metadata_row_groups.rs","mod.rs","row.rs","row_buffer.rs","schema.rs","writer_options.rs"]},{"name":"module","files":["mod.rs","shredder.rs"]}],"files":["lib.rs"]};
 createSourceSidebar();
