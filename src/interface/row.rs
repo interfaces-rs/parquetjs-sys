02cac55c8d9641d83eb38fa8f0229d3af6b66cb1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 19:30:55 -0700

    Bindings for Row interface

--- src/interface/row.rs
+++ src/interface/row.rs
@@ -1,8 +1,9 @@
+use js_sys::Map;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern {
-    #[wasm_bindgen]
-    #[derive(Clone, Debug, PartialEq)]
+    #[wasm_bindgen(extends = Map)]
+    #[derive(Clone, Debug, Eq, PartialEq)]
     pub type Row;
 }
