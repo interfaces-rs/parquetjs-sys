70ac1d3b2d85b3b3d8370d718be35a30b514d9db
darinmorrison <darinmorrison@users.noreply.github.com>
2019-12-01 19:42:52 +0000

    Automated deployment: Sun Dec  1 19:42:52 UTC 2019 38b730eacb51bb1889482662adc3620474c4d061

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["parquetjs_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"parquetjs_sys/struct.Row.html\" title=\"struct parquetjs_sys::Row\">Row</a>",synthetic:false,types:["parquetjs_sys::interface::row::Row"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"parquetjs_sys/struct.WriterOptions.html\" title=\"struct parquetjs_sys::WriterOptions\">WriterOptions</a>",synthetic:false,types:["parquetjs_sys::interface::writer_options::WriterOptions"]},];
+implementors["parquetjs_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"parquetjs_sys/struct.WriterOptions.html\" title=\"struct parquetjs_sys::WriterOptions\">WriterOptions</a>",synthetic:false,types:["parquetjs_sys::interface::writer_options::WriterOptions"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
