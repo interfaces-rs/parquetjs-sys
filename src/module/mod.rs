795464f11f6a22647f4d5163ba21b4fdde6a9cf1
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 19:31:46 -0700

    Bindings for Shredder module

--- src/module/mod.rs
+++ src/module/mod.rs
@@ -1,1 +1,3 @@
+pub(crate) mod shredder;
 
+pub use shredder::*;
---
+++ src/module/shredder.rs
@@ -0,0 +1,16 @@
+use crate::interface::{Row, RowBuffer, Schema};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "electron")]
+extern {
+    #[derive(Clone, Debug, PartialEq)]
+    pub type Shredder;
+
+    pub static shredder: Shredder;
+
+    #[wasm_bindgen(method, js_name = "shredRecord")]
+    pub fn shred_record(this: &Shredder, schema: &Schema, record: &Row, buffer: &RowBuffer);
+
+    #[wasm_bindgen(method, js_name = "materializeRecords")]
+    pub fn materialize_records(this: &Shredder, schema: &Schema, buffer: &RowBuffer);
+}
