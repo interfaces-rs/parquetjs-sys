19cdfb1175a707f256d9c556bacaf4a6c37c2e58
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 19:31:06 -0700

    Bindings for RowBuffer interface

--- src/interface/row_buffer.rs
+++ src/interface/row_buffer.rs
@@ -1,3 +1,4 @@
+use js_sys::Map;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -5,4 +6,10 @@ extern {
     #[wasm_bindgen]
     #[derive(Clone, Debug, PartialEq)]
     pub type RowBuffer;
+
+    #[wasm_bindgen(getter, js_name = "column_data")]
+    pub fn column_data(this: &RowBuffer) -> Map;
+
+    #[wasm_bindgen(getter, js_name = "rowCount")]
+    pub fn row_count(this: &RowBuffer) -> usize;
 }
