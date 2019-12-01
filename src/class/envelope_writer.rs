d547f59472483ca048eb649fe2797bcc0844f7dc
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 19:32:43 -0700

    Bindings for EnvelopeWriter class

--- src/class/envelope_writer.rs
+++ src/class/envelope_writer.rs
@@ -1,3 +1,9 @@
+use crate::{
+    class::Schema,
+    interface::{RowBuffer, WriterOptions},
+};
+use js_sys::{Function, Map, Promise};
+use node_sys::{Buffer, WriteStream};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -12,11 +18,11 @@ extern {
 
     #[wasm_bindgen(constructor)]
     pub fn new(
-        schema: &JsValue,
-        write_fn: &JsValue,
-        close_fn: &JsValue,
-        file_offset: &JsValue,
-        opts: &JsValue,
+        schema: &Schema,
+        write_fn: &Function,
+        close_fn: &Function,
+        file_offset: usize,
+        opts: WriterOptions,
     ) -> EnvelopeWriter;
 
     //****************//
@@ -24,7 +30,7 @@ extern {
     //****************//
 
     #[wasm_bindgen(static_method_of = EnvelopeWriter, js_name = "openStream")]
-    pub fn open_stream(schema: &JsValue, output_stream: &JsValue, opts: &JsValue) -> EnvelopeWriter;
+    pub fn open_stream(schema: &Schema, output_stream: &WriteStream, opts: WriterOptions) -> EnvelopeWriter;
 
     //******************//
     // Instance Methods //
@@ -34,14 +40,14 @@ extern {
     pub fn set_page_size(this: &EnvelopeWriter, count: usize);
 
     #[wasm_bindgen(method, js_name = "writeFooter")]
-    pub fn write_footer(this: &EnvelopeWriter, user_metadata: &JsValue) -> JsValue;
+    pub fn write_footer(this: &EnvelopeWriter, user_metadata: &Map) -> Promise;
 
     #[wasm_bindgen(method, js_name = "writeHeader")]
-    pub fn write_header(this: &EnvelopeWriter) -> JsValue;
+    pub fn write_header(this: &EnvelopeWriter) -> Promise;
 
     #[wasm_bindgen(method, js_name = "writeRowGroup")]
-    pub fn write_row_group(this: &EnvelopeWriter, records: &JsValue) -> JsValue;
+    pub fn write_row_group(this: &EnvelopeWriter, records: &RowBuffer) -> Promise;
 
     #[wasm_bindgen(method, js_name = "writeSection")]
-    pub fn write_section(this: &EnvelopeWriter, buf: &JsValue);
+    pub fn write_section(this: &EnvelopeWriter, buf: &Buffer);
 }
