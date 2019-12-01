da1fd2ab2d865f44d2a513f99ca24dd39db6f368
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 19:30:41 -0700

    Bindings for Metadata interface

--- src/interface/metadata.rs
+++ src/interface/metadata.rs
@@ -1,3 +1,4 @@
+use js_sys::{Array, JsString, Number};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -5,4 +6,24 @@ extern {
     #[wasm_bindgen]
     #[derive(Clone, Debug, PartialEq)]
     pub type Metadata;
+
+    // FIXME: are these readonly?
+
+    #[wasm_bindgen(getter, js_name = "createdBy")]
+    pub fn created_by(this: &Metadata) -> JsString;
+
+    #[wasm_bindgen(getter, js_name = "keyValueMetadata")]
+    pub fn key_value_metadata(this: &Metadata) -> Array;
+
+    #[wasm_bindgen(getter, js_name = "numRows")]
+    pub fn num_rows(this: &Metadata) -> usize;
+
+    #[wasm_bindgen(getter, js_name = "rowGroups")]
+    pub fn row_groups(this: &Metadata) -> Array;
+
+    #[wasm_bindgen(getter)]
+    pub fn schema(this: &Metadata) -> Array;
+
+    #[wasm_bindgen(getter)]
+    pub fn version(this: &Metadata) -> Number;
 }
