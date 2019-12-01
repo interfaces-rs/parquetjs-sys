ad872babcd68546da0aeed57639090cded4d211b
Darin Morrison <darinmorrison@users.noreply.github.com>
2019-11-30 19:32:34 -0700

    Bindings for Reader class

--- src/class/reader.rs
+++ src/class/reader.rs
@@ -1,5 +1,8 @@
-use crate::class::Cursor;
-use js_sys::Promise;
+use crate::{
+    class::{Cursor, EnvelopeReader, Schema},
+    interface::Metadata,
+};
+use js_sys::{Array, JsString, Map, Promise};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -13,14 +16,14 @@ extern {
     //*************//
 
     #[wasm_bindgen(constructor)]
-    pub fn new(metadata: &JsValue, envelope_reader: &JsValue) -> Reader;
+    pub fn new(metadata: &Metadata, envelope_reader: &EnvelopeReader) -> Reader;
 
     //****************//
     // Static Methods //
     //****************//
 
     #[wasm_bindgen(static_method_of = Reader, js_name = "openFile")]
-    pub fn open_file(file_path: &JsValue) -> Promise;
+    pub fn open_file(file_path: &JsString) -> Promise;
 
     //******************//
     // Instance Methods //
@@ -30,14 +33,27 @@ extern {
     pub fn close(this: &Reader) -> Promise;
 
     #[wasm_bindgen(method, js_name = "getCursor")]
-    pub fn get_cursor(this: &Reader, column_list: &JsValue) -> Cursor;
+    pub fn get_cursor(this: &Reader, column_list: &Array) -> Cursor;
 
     #[wasm_bindgen(method, js_name = "getMetadata")]
-    pub fn get_metadata(this: &Reader) -> JsValue;
+    pub fn get_metadata(this: &Reader) -> Map;
 
     #[wasm_bindgen(method, js_name = "getRowCount")]
     pub fn get_row_count(this: &Reader) -> usize;
 
     #[wasm_bindgen(method, js_name = "getSchema")]
-    pub fn get_schema(this: &Reader) -> JsValue;
+    pub fn get_schema(this: &Reader) -> Schema;
+
+    //*********************//
+    // Instance Properties //
+    //*********************//
+
+    #[wasm_bindgen(method, getter)]
+    pub fn envelope_reader(this: &Reader) -> EnvelopeReader;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn metadata(this: &Reader) -> Metadata;
+
+    #[wasm_bindgen(method, getter)]
+    pub fn schema(this: &Reader) -> Schema;
 }
