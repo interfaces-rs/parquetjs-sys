77cb21b1b0328fa4b9709ba8074e4e35ba9d5732
darinmorrison <darinmorrison@users.noreply.github.com>
2019-12-20 01:17:22 +0000

    Automated deployment: Fri Dec 20 01:17:22 UTC 2019 c7739621f819e96cba569d0c3148449ece015f57

---
+++ implementors/core/marker/trait.StructuralEq.js
@@ -0,0 +1,9 @@
+(function() {var implementors = {};
+implementors["parquetjs_sys"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.StructuralEq.html\" title=\"trait core::marker::StructuralEq\">StructuralEq</a> for <a class=\"struct\" href=\"parquetjs_sys/struct.WriterOptions.html\" title=\"struct parquetjs_sys::WriterOptions\">WriterOptions</a>",synthetic:false,types:["parquetjs_sys::interface::writer_options::WriterOptions"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        })()
